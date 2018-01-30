e90c9d504fb0adfcb8ecda617cc2821a3ca9d980
Kamila Skonieczna <95kamila.skonieczna@gmial.com>
2018-01-30 22:32:03 +0100

    Added new task functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
     //T A S K 1 - validation and saving tasks
@@ -10,7 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var priority = document.getElementById("priority");
     var form = document.getElementById("formTodo");
     var tasks = [];
+    var id = 1;
 
+
+    //F U N C T I O N S
     function hideForm() {
         form.hidden = true;
     }
@@ -27,26 +32,57 @@ document.addEventListener('DOMContentLoaded', function () {
         return true;
     }
 
-    function createObject() {
-        id = 1;
-        tasks.push(
-            {
+    function createTaskObject() {
+       return {
                 id: id++,
                 title: taskNameField.value,
                 date: dateField.value,
                 priority: priority.value,
                 description: descriptionField.value,
                 done: false
-            }
-        );
+       }
     }
 
-    // function createNewTask() {
-    //     var newTask = document.createElement("li");
-    //     var
-    //
-    // }
+    function create(tag, className, content) {
+        var element = document.createElement(tag);
+        if(className) {
+            element.className = className;
+        }
+        if(content) {
+            element.innerHTML = content;
+        }
+        return element;
+    }
 
+    function createNewTask() {
+        var taskObj = createTaskObject();
+
+        var newLi = create("li", "ui-state-default");
+        var newLabel = create("label", "");
+        var newCheckbox = create("input");
+        newCheckbox.setAttribute("type","checkbox");
+        var todoInfoBox = create("div", "todoInfo");
+        var descriptionBox = create("p", "", taskObj.description);
+        var priorityValueName = create("p", "valueName", "priority ");
+        var dateValueName = create("p", "valueName", "date ");
+        var priorityValue = create("span", "priorityValue", taskObj.priority);
+        var dateValue = create("span", "dateValue", taskObj.date);
+        var hrElement = create("hr");
+
+        var listTodos = document.getElementById("listTodos");
+        listTodos.appendChild(hrElement);
+        listTodos.appendChild(newLi);
+        newLi.appendChild(newLabel);
+        newLabel.appendChild(newCheckbox);
+        var titleText = document.createTextNode(taskObj.title);
+        newLi.appendChild(titleText);
+        newLi.appendChild(todoInfoBox);
+        todoInfoBox.appendChild(descriptionBox);
+        todoInfoBox.appendChild(priorityValueName);
+        priorityValueName.appendChild(priorityValue);
+        todoInfoBox.appendChild(dateValueName);
+        dateValueName.appendChild(dateValue);
+    }
 
     function clearInput() {
         taskNameField.value = "";
@@ -54,18 +90,19 @@ document.addEventListener('DOMContentLoaded', function () {
         priority.value = priorityOptions[0].value;
     }
 
-
+    //D E F A U L T
     hideForm();
     addButton.innerText = addButton.dataset.title;
 
+    //E V E N T
     addButton.addEventListener("click", function () {
         form.hidden = !form.hidden;
         if (!form.hidden) {
             this.innerText = "add";
         } else {
             if (validate()) {
+                createNewTask();
                 hideForm();
-                createObject();
                 addButton.innerText = addButton.dataset.title;
             } else {
                 showForm();
