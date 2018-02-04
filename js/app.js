9df6c76538ae0fc8e30a5a5487fdfeef801c0b62
Monika Cilińska <cilinskam@yahoo.com>
2018-02-04 12:30:47 +0100

    refactor of changeButtonsColor function

--- js/app.js
+++ js/app.js
@@ -288,51 +288,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //font color & animation - next
 
+    function changeElementClass(element, className, classIndex){
+        element.classList.remove(className + "-" + (classIndex-1)%3);
+        element.classList.remove(className + "-" + (classIndex+1)%3);
+        element.classList.add(className + "-" + classIndex%3);
+    }
+
+    function changeElementsClass(array, className, classIndex){
+        for(var i = 0; i < array.length; i++){
+            changeElementClass(array[i], className, classIndex);
+        }
+    }
+
     function changeButtonsColor(classIndex){
         var valueName = document.querySelectorAll(".valueName");
         var hr = document.querySelectorAll("hr");
         var colorButtons = document.querySelectorAll(".btn.mark");
-        for (var i = 0; i < colorButtons.length; i++) {
-          colorButtons[i].classList.remove(`new-wave-${(classIndex-1)%3}`);
-          colorButtons[i].classList.remove(`new-wave-${(classIndex+1)%3}`);
-          colorButtons[i].classList.add(`new-wave-${classIndex%3}`);
-        }
-        for (var i = 0; i < hr.length; i++) {
-          hr[i].classList.remove(`new-bckg-${(classIndex-1)%3}`);
-          hr[i].classList.remove(`new-bckg-${(classIndex+1)%3}`);
-          hr[i].classList.add(`new-bckg-${(classIndex)%3}`);
-        }
-        for (var i = 0; i < valueName.length; i++) {
-          valueName[i].classList.remove(`new-color-${(classIndex-1)%3}`);
-          valueName[i].classList.remove(`new-color-${(classIndex+1)%3}`);
-          valueName[i].classList.add(`new-color-${(classIndex)%3}`);
-        }
-        for (var i = 0; i < fas.length; i++) {
-          fas[i].classList.remove(`new-border-color-${(classIndex-1)%3}`);
-          fas[i].classList.remove(`new-border-color-${(classIndex+1)%3}`);
-          fas[i].classList.add(`new-border-color-${classIndex%3}`);
-        }
-        h1.classList.remove(`new-color-${(classIndex-1)%3}`);
-        h1.classList.remove(`new-color-${(classIndex+1)%3}`);
-        h1.classList.add(`new-color-${(classIndex)%3}`);
-        changeStyle.classList.remove(`new-color-${(classIndex-1)%3}`);
-        changeStyle.classList.remove(`new-color-${(classIndex+1)%3}`);
-        changeStyle.classList.add(`new-color-${(classIndex)%3}`);
-        optionsChangeStyle[0].classList.remove(`new-color-${(classIndex-1)%3}`);
-        optionsChangeStyle[0].classList.remove(`new-color-${(classIndex+1)%3}`);
-        optionsChangeStyle[0].classList.add(`new-color-${(classIndex)%3}`);
-        optionsChangeStyle[1].classList.remove(`new-color-${(classIndex-1)%3}`);
-        optionsChangeStyle[1].classList.remove(`new-color-${(classIndex+1)%3}`);
-        optionsChangeStyle[1].classList.add(`new-color-${(classIndex)%3}`);
-        sortType[0].classList.remove(`new-color-${(classIndex-1)%3}`);
-        sortType[0].classList.remove(`new-color-${(classIndex+1)%3}`);
-        sortType[0].classList.add(`new-color-${(classIndex)%3}`);
-        sortType[1].classList.remove(`new-color-${(classIndex-1)%3}`);
-        sortType[1].classList.remove(`new-color-${(classIndex+1)%3}`);
-        sortType[1].classList.add(`new-color-${(classIndex)%3}`);
-        todolist.classList.remove(`new-main-border-${(classIndex-1)%3}`);
-        todolist.classList.remove(`new-main-border-${(classIndex+1)%3}`);
-        todolist.classList.add(`new-main-border-${(classIndex)%3}`);
+
+        changeElementsClass(colorButtons, 'new-wave', classIndex);
+        changeElementsClass(hr, 'new-bckg', classIndex);
+        changeElementsClass(valueName, 'new-color', classIndex);
+        changeElementsClass(fas, 'new-border-color', classIndex);
+        changeElementsClass(sortType, 'new-color', classIndex);
+        changeElementsClass(optionsChangeStyle, 'new-color', classIndex);
+
+        changeElementClass(h1, 'new-color', classIndex);
+        changeElementClass(changeStyle, 'new-color', classIndex);
+        changeElementClass(todolist, 'new-main-border', classIndex);
     }
 
     nextColorButtons.addEventListener("click", function() {
